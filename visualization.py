6c06cbcfa91ae51b2e53cdff3072c19e7feb19f6
noah marestaing <noahmarestaing@gmail.com>
2023-11-13 19:07:46 +0000

    update titles

--- visualization.py
+++ visualization.py
@@ -2,18 +2,18 @@ import plotly.graph_objects as go
 
 import pandas as pd
 # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
-df = pd.read_csv('visited_states.csv')
+df = pd.read_csv('https://github.com/marestaing/hosting/blob/main/visited_states.csv?raw=true')
 
 fig = go.Figure(data=go.Choropleth(
     locations=df['code'], # Spatial coordinates
     z = df['number students'].astype(float), # Data to be color-coded
     locationmode = 'USA-states', # set of locations match entries in `locations`
     colorscale = 'Blues',
-    colorbar_title = "Number of Students",
+    colorbar_title = "color scale title",
 ))
 
 fig.update_layout(
-    title_text = '2011 US Agriculture Exports by State',
+    title_text = 'title here',
     geo_scope='usa', # limite map scope to USA
 )
 
