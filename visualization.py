d10b91e79e64ecf7f4bab7b3b9b698e8b5c4a8db
noah marestaing <noahmarestaing@gmail.com>
2023-11-13 17:42:43 +0000

    fetching file

--- visualization.py
+++ visualization.py
@@ -1,2 +1,20 @@
-print("hello")
+import plotly.graph_objects as go
 
+import pandas as pd
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
+df = pd.read_csv('visited_states.csv')
+
+fig = go.Figure(data=go.Choropleth(
+    locations=df['code'], # Spatial coordinates
+    z = df['number students'].astype(float), # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = 'Blues',
+    colorbar_title = "Number of Students",
+))
+
+fig.update_layout(
+    title_text = '2011 US Agriculture Exports by State',
+    geo_scope='usa', # limite map scope to USA
+)
+
+fig.show()
