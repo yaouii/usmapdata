ed2af0f9b2b5af7af875bc81bc67f442bf266411
bread sticks <uubixx.0208@gmail.com>
2023-11-15 20:56:13 +0000

    changed colors and the title

--- visualization.py
+++ visualization.py
@@ -2,18 +2,18 @@ import plotly.graph_objects as go
 
 import pandas as pd
 
-df = pd.read_csv('url goes here')
+df = pd.read_csv('https://github.com/marestaing/hosting/blob/main/visited_states.csv?raw=true')
 
 fig = go.Figure(data=go.Choropleth(
     locations=df['code'], # Spatial coordinates
     z = df['number students'].astype(float), # Data to be color-coded
     locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = 'Blues',
-    colorbar_title = "color scale title",
+    colorscale = 'dense',
+    colorbar_title = "most to least visited states",
 ))
 
 fig.update_layout(
-    title_text = 'title here',
+    title_text = 'ap computer science u.s. map cool',
     geo_scope='usa', # limite map scope to USA
 )
 
